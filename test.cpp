f27ae260ac6a14853c21a511188ce21309982353
diego-mtus <dmatus2023@udec.cl>
2026-05-09 18:52:56 -0400

    archivo test.cpp añadido para pruebas de funcionamiento

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cassert>
+#include "tree.hpp"
+#include "parser_datos.hpp"
+
+int main()
+{
+    Tree miArbol;
+    std::string rutaXml = "./dataset";
+
+    ParserDatos::cargarCarpeta(rutaXml, miArbol);
+
+    // PRUEBA DE BÚSQUEDA
+    int idFinal = 142473;
+    if (miArbol.search(miArbol.root(), idFinal) != nullptr)
+    {
+        std::cout << "ID " << idFinal << " encontrado ANTES de borrar." << std::endl;
+    }
+
+    // 3. PRUEBA DE PRECURSORES
+    int idVisual = 142473;
+    std::cout << "Evaluando precursores para ID " << idVisual << ":" << std::endl;
+    miArbol.precursores(idVisual);
+
+    miArbol.desplegar_datos(idVisual); // Mostrar datos
+
+    double filtro = 4.6;
+    miArbol.borrar_ratings(filtro);
+    std::cout << "Nuevo tamaño tras borrar ratings <= 4.6: " << miArbol.size() << std::endl;
+
+    miArbol.listar(); // Listar para verificar que se borraron los nodos correctos
+
+    // Código para encontrar precursores en todo el árbol
+    std::vector<int> todos_los_ids = miArbol.preOrder();
+    std::cout << "Buscando precursores en el dataset..." << std::endl;
+
+    for (int id : todos_los_ids)
+    {
+        if (id != -1)
+        { // Ignorar raíz
+            miArbol.precursores(id);
+        }
+    }
+
+    miArbol.desplegar_datos(280111); // Mostrar datos del ID 280111
+    return 0;
+}
