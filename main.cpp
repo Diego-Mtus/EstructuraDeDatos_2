1b37a6f9af03631ca9cfb981d22031ae9471bafb
diego-mtus <dmatus2023@udec.cl>
2026-06-02 01:26:48 -0400

    Se añadieron outputs de ejemplo del funcionamiento del programa

--- main.cpp
+++ main.cpp
@@ -13,12 +13,29 @@ int main()
     // ------------------------------------------------
     // De aquí en adelante meter funciones para probar
 
+    std::cout << "Tamaño del árbol inicial: " << arbolMain.size() << std::endl;
+
+    std::cout << "Se borran los libros con rating promedio <= 4.4" << std::endl;
+
     arbolMain.borrar_ratings(4.4);
 
     arbolMain.precursores();
 
     arbolMain.listar();
+
     std::cout << "Tamaño del árbol: " << arbolMain.size() << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Ahora con un rating más alto: 4.7" << std::endl;
+
+    arbolMain.borrar_ratings(4.7);
+    arbolMain.precursores();
+    arbolMain.listar();
+    std::cout << "Tamaño del árbol: " << arbolMain.size() << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    for(int id : arbolMain.preOrder()) {
+        arbolMain.desplegar_datos(id);
+        std::cout << "-----------------------------" << std::endl;
+    }
 
     return 0;
 }
